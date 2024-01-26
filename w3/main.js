5cd65f081a8dd66251fa99bd9bcad6e826d7d711
Rakram4545 <rabeeza45@mail.fresnostate.edu>
2024-01-25 16:16:42 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,10 +1,5 @@
 function determineHouseHoldPts() {
     console.log("Inside the function")
-}
-
-let CarbonFootprintPoints = 0;
-const numberInHousehold = 3;
-
 if (numberInHousehold === 1) {
     CarbonFootprintPoints = CarbonFootprintPoints + 14;
 } else if(numberInHousehold === 2) {
@@ -15,14 +10,27 @@ if (numberInHousehold === 1) {
     CarbonFootprintPoints = CarbonFootprintPoints + 8;
 } else if(numberInHousehold === 5) {
     CarbonFootprintPoints = CarbonFootprintPoints + 6;
-} esle if(numberInHousehold === 6) {
+} else if(numberInHousehold === 6) {
     CarbonFootprintPoints = CarbonFootprintPoints + 4;
 } else if(numberInHousehold > 6) {
     CarbonFootprintPoints = CarbonFootprintPoints + 2; 
 }
-console.log(`Based on the number of members in household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.`); 
+console.log(`Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.`);
+}
 
-cl
+let CarbonFootprintPoints = 0;
+const numberInHousehold = 3;
 
 
-// global scope
+//global scope
+
+determineHouseHoldPts(3)
+determineHouseHoldPts(4)
+
+
+   
+
+
+
+
+
