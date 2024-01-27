a0d72665946694fb7de501106aaed541467f4b4c
Rakram4545 <rabeeza45@mail.fresnostate.edu>
2024-01-26 17:23:27 -0800

    setup done

--- w3/main.js
+++ w3/main.js
@@ -1,6 +1,6 @@
-function determineHouseHoldPts() {
-    console.log("Inside the function")
-if (numberInHousehold === 1) {
+/*
+function determineHouseHoldPts(numberInHousehold) {
+if(numberInHousehold === 1) {
     CarbonFootprintPoints = CarbonFootprintPoints + 14;
 } else if(numberInHousehold === 2) {
     CarbonFootprintPoints = CarbonFootprintPoints + 12;
@@ -19,15 +19,19 @@ console.log(`Based on the number of members in the household of ${numberInHouseh
 }
 
 let CarbonFootprintPoints = 0;
-const numberInHousehold = 3;
-
-
-//global scope
+//const numberInHousehold = 3;
 
 determineHouseHoldPts(3)
 determineHouseHoldPts(4)
 
-if (Housesize === "Largehousesize") {
+*/
+// If you have large house, then add 10 points to your score.
+// If you have medium-sized house, then add 7 points.
+// If you have small house, then add 4 points.
+// IF you live in an apartment, then add 2 points.
+
+function determineHousesizePts(houseSize) {
+if(Housesize === "Largehousesize") {
     cardonFootprintPoints = CarbonFootprintPoints + 10;
 } else if(housesize === "Mediumsizehouse") {
     cardonFootprintPoints = cardonFootprintPoints + 7;
@@ -36,9 +40,9 @@ if (Housesize === "Largehousesize") {
 } else if(Housesize === "Apartmentsize") {
     cardonFootprintPoints = cardonFootprintPoints + 2;
 }
-   
+ console.log(`Based on the house size of ${housesize} the total points would be ${carbonFootprintPoints}.`);{
+
+ }
 
 
-
-
-
+ findingHousesize("apartment")
