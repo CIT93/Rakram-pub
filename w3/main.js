9bd02e0a4f720ed349a5215fb901e842d5a47cbe
Rakram4545 <rabeeza45@mail.fresnostate.edu>
2024-02-01 00:49:24 -0800

    setup and display done

--- w3/main.js
+++ w3/main.js
@@ -1,33 +1,35 @@
-function determineHouseholdPts(size) {
-   if (size === "large") {
+function determineHouseholdPts(numberInHousehold) {
+   console.log("Inside the function")
+   if (numberInHousehold === 1) {
+    cardonFootprintPoint = cardonFootprintPoint + 14;
+   } else if (numberInHousehold === 2) {
+    cardonFootprintPoint = cardonFootprintPoint + 12;
+   } else if (numberInHousehold === 3) {
     cardonFootprintPoint = cardonFootprintPoint + 10;
-   } else if (size === "meduim") {
-    cardonFootprintPoint = cardonFootprintPoint + 7;
-   } else if (size === "small") {
+   } else if (numberInHousehold === 4) {
+    cardonFootprintPoint = cardonFootprintPoint + 8;
+   } else if (numberInHousehold === 5) {
+    cardonFootprintPoint = cardonFootprintPoint + 6;
+   } else if (numberInHousehold === 6) {
     cardonFootprintPoint = cardonFootprintPoint + 4;
+   } else if (numberInHousehold > 6) {
+    cardonFootprintPoint = cardonFootprintPoint + 2;
    }
-   else if (size === "apt") {
-    cardonFootprintPoint = cardonFootprintPoint + 4;
-   }
-}
+
 
 function determineHouseholdPts(numberInHousehold) {
-console.log("Inside the function")
-    if (numberInHousehold === 1) {
-        cardonFootprintPoint = cardonFootprintPoint + 14;
-    } else if (numberInHousehold === 2) {
-        cardonFootprintPoint = cardonFootprintPoint + 12;
-    } else if (numberInHousehold === 3) {
+    if (size === "large") {
         cardonFootprintPoint = cardonFootprintPoint + 10;
-    } else if (numberInHousehold === 4) {
-        cardonFootprintPoint = cardonFootprintPoint + 8;
-    } else if (numberInHousehold === 5) {
-        cardonFootprintPoint = cardonFootprintPoint + 6;
-    } else if (numberInHousehold === 6) {
+       } else if (size === "meduim") {
+        cardonFootprintPoint = cardonFootprintPoint + 7;
+       } else if (size === "small") {
         cardonFootprintPoint = cardonFootprintPoint + 4;
-    } else if (numberInHousehold > 6) {
-        cardonFootprintPoint = cardonFootprintPoint + 2;
-    }
+       }
+       else if (size === "apt") {
+        cardonFootprintPoint = cardonFootprintPoint + 4;
+       }
+    }  
+
     console.log(
         `Based on the number of members in the household of ${numberInHousehold} the point would be ${cardonFootprintPoint}.`);
 }
---
+++ w4/main.js
@@ -0,0 +1,59 @@
+const CFPDATA = [];
+
+function determineHouseSizePts(size) {
+    console.log("inside block scope");
+    let houseSizePoints = 0;
+    if (size === "large") {
+        houseSizePoints = 10;
+    } else if (size === "medium") {
+        houseSizePoints = 7;
+    } else if (size === "small") {
+        houseSizePoints = 4;
+    } else if (size === "apt") {
+        houseSizePoints = 2;
+    } 
+    return houseSizePoints;
+}
+
+function determineHouseholdnumbers(numberInHousehold) {
+  let houseHoldpoints = 0;
+  if (numberInHousehold === 1) {
+    houseHoldPoints = 14;
+  } else if (numberInHousehold === 2) {
+    houseHoldPoints = 12;
+  } else if (numberInHousehold === 3) {
+   houseHoldPoints = 10;
+  } else if (numberInHousehold === 4) {
+    houseHoldpoints = 8;
+  } else if (numberInHousehold === 5) {
+    houseHoldpoints = 6;
+  } else if (numberInHousehold === 6) {
+   houseHoldpoints = 4;
+  } else if (numberInHousehold > 6) {
+   houseHoldpoints = 2;
+  }
+    return houseHoldpoints
+}
+
+function start(houseHoldMembers, houseSize) {
+    const householdPts =  determineHouseholdnumbers(houseHoldMembers);
+    const householdsize = determineHouseSizePts(houseSize);
+    const total = householdsize + householdPts;
+    CFPDATA.push([houseHoldMembers, houseSize, householdPts, houseSize, total])
+  }
+
+  function displayOutput() {
+
+  }
+
+    start(5, "apt");
+    start(4, "large");
+    start(3, "meduim");
+    start(2, "small");
+
+    displayOutput()
+    
+    
+
+
+
